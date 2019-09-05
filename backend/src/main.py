4a1a50560041909d0384bf7363d0bf6f39a27844
Anthony Winters <a.r.winters@nforce-it.nl>
2019-09-05 22:24:24 +0200

    Add CORS support Flask

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from .entities.entity import Session, engine, Base
 from .entities.playbook import Playbook, PlaybookSchema
 
 app = Flask(__name__)
+CORS(app)
 
 # generate database schema
 Base.metadata.create_all(engine)
